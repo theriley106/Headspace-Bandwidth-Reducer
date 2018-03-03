6f19b9049a39a62a8f27ecc631140b502bee0a1f
Theriley106 <christopherlambert106@gmail.com>
2018-03-03 08:17:59 -0500

    Added base for the app.py file that will eventually contain the web app

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, request, render_template, request, url_for, redirect, Markup, Response, send_file, send_from_directory, make_response
+import os
+import time
+
+app = Flask(__name__)
+
+
+if __name__ == "__main__":
+	pass
