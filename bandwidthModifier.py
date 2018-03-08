335526d5834705179c4d1b23ac08c33c384731e6
Theriley106 <christopherlambert106@gmail.com>
2018-03-08 17:13:14 -0500

    Added timestamp split

--- bandwidthModifier.py
+++ bandwidthModifier.py
@@ -1,7 +1,21 @@
 import os
+import commands
+import re
+
+def getSilenceTimestamps(audioFile):
+	splitPoints = []
+	output, tmp = commands.getstatusoutput("ffmpeg -i {} -af silencedetect=noise=-50dB:d=2 -f null -".format(audioFile))
+	#output, tmp = commands.getstatusoutput("sox -V3 {} newAudio.mp3 silence -l 1 0.0 -50d 1 1.0 -50d : newfile : restart".format(audioFile))
+	for var in str(tmp).split("\n"):
+		if "_end" in str(var):
+			end, duration = re.findall("\d+\.\d+", str(var))
+			splitPoints.append({"Start": float(end) - float(duration), "End": float(end)})
+	return splitPoints
 
 def splitAudio(audioFile):
-	os.system("sox -V3 {} newAudio.mp3 silence 1 0.5 2% 1 2.0 2% : newfile : restart".format(audioFile))
-
+	timeStamps = getSilenceTimestamps(audioFile)
+	ffmpeg -ss <silence_end - 0.25> -t <next_silence_start - silence_end + 0.25> -i
 if __name__ == '__main__':
-	splitAudio(raw_input("Audio File: "))
+	print getSilenceTimestamps("3MinuteBasics.mp3")
+
+#ffmpeg -ss <silence_end - 0.25> -t <next_silence_start - silence_end + 0.25> -i input.mov word-N.mov
