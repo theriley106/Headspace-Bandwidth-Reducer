4945ae5b27ae783f5b0de3b25b7d508420c443ee
Theriley106 <christopherlambert106@gmail.com>
2018-03-08 16:44:49 -0500

    Much better way of splitting audio

--- bandwidthModifier.py
+++ bandwidthModifier.py
@@ -1,7 +1,7 @@
 import os
 
 def splitAudio(audioFile):
-	os.system("sox -V3 {} newAudio.mp3 silence 1 3.0 0.1% 1 0.3 0.1% : newfile : restart".format(audioFile))
+	os.system("sox -V3 {} newAudio.mp3 silence 1 0.5 2% 1 2.0 2% : newfile : restart".format(audioFile))
 
 if __name__ == '__main__':
 	splitAudio(raw_input("Audio File: "))
