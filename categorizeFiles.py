22a8bd21ca7cfe84a2555dcde09cfc00cb525041
Theriley106 <christopherlambert106@gmail.com>
2018-03-13 21:08:12 -0400

    Added regex to extract time

--- categorizeFiles.py
+++ categorizeFiles.py
@@ -2,7 +2,12 @@ import bandwidthModifier as bM
 import re
 
 def extractTime(string):
-	return re.findall("the-basics_s\d+_(\d+)", string)
+	# Regex to extract time from file
+	return re.findall("the-basics_s\d+_(\d+)", string)[0]
+
+def extractDay(string):
+	# Regex to extract day from file
+	return re.findall("the-basics_s(\d+)_", string)[0]
 
 for val in bM.findAllMp3('Mp3/'):
-	print extractTime(val)
+	print extractDay(val)
