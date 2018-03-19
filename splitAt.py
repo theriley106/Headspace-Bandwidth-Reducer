5ccd33e1d7b18a9b87ecfca0f01b2271ca3be500
Theriley106 <christopherlambert106@gmail.com>
2018-03-19 17:50:58 -0400

    Split at prints all json values

---
+++ splitAt.py
@@ -0,0 +1,36 @@
+import os
+import json
+import re
+
+def checkText(text):
+    return int(text) if text.isdigit() else text
+
+def sortKey(fileName):
+	head, tail = os.path.split(fileName)
+	return [ checkText(c) for c in re.split('(\d+)', tail) ]
+
+def findAllJson(directory):
+	listOfFiles = []
+	for file in os.listdir(directory):
+		if file.endswith(".json"):
+			listOfFiles.append(os.path.join(directory, file))
+	listOfFiles.sort(key=sortKey)
+	return listOfFiles
+
+def genNew(jsonFile):
+	directory = jsonFile[::-1].format('/')[2][::-1]
+	print directory
+	num = jsonFile.replace(directory, "").replace(".json", "")
+	prevTime = 0.0
+	os.system("rm -rf {}".format(jsonFile.replace(".json", "")))
+	os.system("mkdir {}".format(jsonFile.replace(".json", "")))
+	for i, val in enumerate(json.load(open(jsonFile))):
+		if float(val["Start"]) < float(prevTime):
+			raw_input("continue? " + str(val["Start"]) + " : " + str(prevTime))
+		os.system("ffmpeg -i {}.mp3 -c copy -ss {} -to {} {}/{}.mp3".format(num, prevTime, val["Start"], num, i))
+		prevTime = val['End']
+if __name__ == '__main__':
+	#genNew(raw_input("Num: "))
+	for i in range(10):
+		for var in findAllJson('static/Mp3/basics_s{}'.format(i+1)):
+			print var
