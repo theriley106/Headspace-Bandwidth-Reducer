4abe09c95ba4943f8785ad51aeeb5c1fc7dfdc78
Theriley106 <christopherlambert106@gmail.com>
2018-03-19 17:21:40 -0400

    Created a way to find errors within the timing in the json files

--- createJsonFiles.py
+++ createJsonFiles.py
@@ -2,4 +2,12 @@ import bandwidthModifier as bM
 
 for i in range(10):
 	for var in bM.findAllMp3('static/Mp3/basics_s{}'.format(i+1)):
-		print var
+		saveAs = var.replace('.mp3', '.json')
+		silenceInfo = bM.getSilenceTimestamps(var)
+		for e, var in enumerate(silenceInfo):
+			try:
+				if var['Start'] > var["End"] or var['End'] > silenceInfo[e+1]['Start']:
+					print("error found")
+			except:
+				if var['Start'] > var["End"]:
+					print("error found")
