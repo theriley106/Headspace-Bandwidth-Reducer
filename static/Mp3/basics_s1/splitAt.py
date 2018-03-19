545a7b25790f984f1e5d5885851de8b968c5299d
Theriley106 <christopherlambert106@gmail.com>
2018-03-19 14:13:29 -0400

    Restructured javascript to be less messy

--- static/Mp3/basics_s1/splitAt.py
+++ static/Mp3/basics_s1/splitAt.py
@@ -2,5 +2,5 @@ import os
 import json
 prevTime = 0.0
 for i, val in enumerate(json.load(open("3.json"))):
-	os.system("ffmpeg -i 3.mp3 -c copy -ss {} -to {} test/{}.mp3".format(prevTime, val["End"], i))
+	os.system("ffmpeg -i 3.mp3 -c copy -ss {} -to {} test/{}.mp3".format(prevTime, val["Start"], i))
 	prevTime = val['End']
