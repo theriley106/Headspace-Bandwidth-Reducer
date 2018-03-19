0299c31c3b16b265d48bc7ddbf98356e0fe0a518
Theriley106 <christopherlambert106@gmail.com>
2018-03-19 17:37:13 -0400

    Createjson files now works in all situations

--- createJsonFiles.py
+++ createJsonFiles.py
@@ -1,13 +1,14 @@
 import bandwidthModifier as bM
+import json
 
 for i in range(10):
 	for var in bM.findAllMp3('static/Mp3/basics_s{}'.format(i+1)):
 		saveAs = var.replace('.mp3', '.json')
 		silenceInfo = bM.getSilenceTimestamps(var)
+		prevTime = 0.0
 		for e, var in enumerate(silenceInfo):
-			try:
-				if var['Start'] > var["End"] or var['End'] > silenceInfo[e+1]['Start']:
-					print("error found")
-			except:
-				if var['Start'] > var["End"]:
-					print("error found")
+			if float(var["Start"]) < float(prevTime) or float(var['Start']) > float(var["End"]) or abs(float(var['End']) - float(var["Start"])) > 60:
+				silenceInfo.remove(var)
+			prevTime = var['End']
+		with open(saveAs, 'w') as fp:
+			json.dump(silenceInfo, fp)
