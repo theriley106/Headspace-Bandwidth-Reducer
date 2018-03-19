2cb4d022ece36bb8da8a1edeab1c3c43c8451fdf
Theriley106 <christopherlambert106@gmail.com>
2018-03-19 15:31:19 -0400

    Fixed the duration problem where it was more

--- static/Mp3/basics_s1/splitAt.py
+++ static/Mp3/basics_s1/splitAt.py
@@ -7,6 +7,8 @@ def genNew(num):
 	os.system("rm -rf {}".format(num))
 	os.system("mkdir {}".format(num))
 	for i, val in enumerate(json.load(open("{}.json".format(num)))):
+		if float(val["Start"]) < float(prevTime):
+			raw_input("continue? " + str(val["Start"]) + " : " + str(prevTime))
 		os.system("ffmpeg -i {}.mp3 -c copy -ss {} -to {} {}/{}.mp3".format(num, prevTime, val["Start"], num, i))
 		prevTime = val['End']
 if __name__ == '__main__':
