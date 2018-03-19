040989c10e6a24e701f65d4976c4ce54853c79f0
Theriley106 <christopherlambert106@gmail.com>
2018-03-19 15:05:35 -0400

    Splitat modified to not have mkdir in the for loop

--- static/Mp3/basics_s1/splitAt.py
+++ static/Mp3/basics_s1/splitAt.py
@@ -4,9 +4,9 @@ import json
 def genNew(num):
 
 	prevTime = 0.0
+	os.system("rm -rf {}".format(num))
+	os.system("mkdir {}".format(num))
 	for i, val in enumerate(json.load(open("{}.json".format(num)))):
-		os.system("rm -rf {}".format(num))
-		os.system("mkdir {}".format(num))
 		os.system("ffmpeg -i {}.mp3 -c copy -ss {} -to {} {}/{}.mp3".format(num, prevTime, val["Start"], num, i))
 		prevTime = val['End']
 if __name__ == '__main__':
