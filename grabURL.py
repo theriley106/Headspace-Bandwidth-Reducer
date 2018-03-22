13f4a00fc904377b75367a8560203e512e837376
Theriley106 <christopherlambert106@gmail.com>
2018-03-21 20:27:29 -0400

    Added input auth code

--- grabURL.py
+++ grabURL.py
@@ -6,7 +6,7 @@ def generateURL(number):
         'Origin': 'https://my.headspace.com',
         'Accept-Encoding': 'gzip, deflate, br',
         'Accept-Language': 'en-US,es-US;q=0.8,es;q=0.6,ru-BY;q=0.4,ru;q=0.2,en;q=0.2',
-        'authorization': 'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImU3OTRiZmMzLWY1ZTktNDVhNC04Y2E1LWViNWRkYzkwNDIxZiIsInVzZXJJZCI6IkhTVVNFUl9NQ0dRWUlHNFQ5OEdGQU1XIiwicGxhdGZvcm0iOiJERVNLVE9QIiwic2NvcGUiOlsiVVNFUjpIU1VTRVJfTUNHUVlJRzRUOThHRkFNVyJdLCJwcml2aWxlZ2VzIjpbIlNUQU5EQVJEX0NPTlRFTlQiLCJTVUJTQ1JJQkVSIl0sImlhdCI6MTUyMDkxNTk5ODQ2OCwidjJBcGlLZXkiOiJqK2ZzMS9FTWpJWXlyWGlPYUJaU0JweklDSlFxTDM3SmVDaTR5UG93d3dZPSJ9.FzRMq3xHF306qnhSWZM2OISdOMROi83YvSg9Zn2Pc1Y',
+        'authorization': raw_input("Auth Code: "),
         'Accept': 'application/vnd.api+json',
         'Cache-Control': 'no-cache',
         'X-HS-No-Cache': 'true',
